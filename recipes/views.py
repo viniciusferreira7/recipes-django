ae537cf326ab5eb1c187d7a1872bd3d155edef28
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-11-12 15:18:50 -0300

    test: add context to test on templates

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request=request, template_name='home.html')
+    return render(request=request, template_name='recipes/home.html', context={
+        "name": "John Doe"
+    })
 
 
 def contact(request: HttpRequest) -> HttpResponse:
