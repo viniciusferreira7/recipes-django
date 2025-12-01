cde2e194443c24451f3d1157a5ed4ccc27242cb7
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-01 10:07:58 -0300

    feat: implement fake data into recipe card

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from utils.recipe.factory import make_recipe
 
 
 def home(request: HttpRequest) -> HttpResponse:
     return render(
         request=request,
         template_name='recipes/pages/home.html',
+        context={
+            'recipes': [make_recipe() for _ in range(10)]
+        }
     )
 
 
@@ -13,4 +17,7 @@ def recipe(request: HttpRequest, id: int) -> HttpResponse:
     return render(
         request=request,
         template_name='recipes/pages/recipe-view.html',
+        context={
+            'recipe': make_recipe()
+        }
     )
--- utils/recipe/factory.py
+++ utils/recipe/factory.py
@@ -25,6 +25,7 @@ class Cover(TypedDict):
 
 
 class Recipe(TypedDict):
+    id: int
     title: str
     description: str
     preparation_time: int
@@ -38,8 +39,20 @@ class Recipe(TypedDict):
     cover: Cover
 
 
+FOOD_TAGS = [
+    "food", "pizza", "burger", "pasta", "cake", "salad",
+    "steak", "sushi", "sandwich", "dessert", "chocolate",
+    "ramen", "tacos", "soup", "bread", "fruits"
+]
+
+
+def random_food_tag():
+    return fake.random_element(FOOD_TAGS)
+
+
 def make_recipe() -> Recipe:
     return {
+        'id': fake.random_number(digits=2, fix_len=True),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
@@ -56,7 +69,8 @@ def make_recipe() -> Recipe:
             'name': fake.word()
         },
         'cover': {
-            'url': f"https://loremflickr.com/{rand_ratio()[0]}/{rand_ratio()[1]}/food,cook"
+           'url': f"https://loremflickr.com/{rand_ratio()[0]}/{rand_ratio()[1]}/{random_food_tag()}"
+
         }
     }
 
