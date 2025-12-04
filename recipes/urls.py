292e136e1abce318bffcf9fb8d1a759a8f9c57b7
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-04 09:52:44 -0300

    feat: add dynamic links into recipes app

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,9 @@ from typing import List, Union
 from django.urls.resolvers import URLPattern, URLResolver
 from recipes import views
 
+app_name = "recipes"
+
 urlpatterns: List[Union[URLPattern, URLResolver]] = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe)
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe")
 ]
